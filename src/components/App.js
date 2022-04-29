97ec27922d57ab470d1d328f4f6b79a43b07b26e
dheeraj257 <55332301+dheeraj257@users.noreply.github.com>
2022-04-29 06:10:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h1 data-ns-test="project-name">zomato</h1>
+   <p data-ns-test="project-description">zomato is best web app</p>
+    </div>
   )
 }
 
